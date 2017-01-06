402d7be37f82ad0d3b4f308d1f844fe7104b83cc
Becca85 <rebeccamaccio@gmail.com>
2017-01-06 12:20:13 +0100

    mise en page

--- src/AppBundle/Form/DemandeurEmploiType.php
+++ src/AppBundle/Form/DemandeurEmploiType.php
@@ -13,7 +13,39 @@ class DemandeurEmploiType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('dateInscriptionDe')->add('dateDerniereModifDe')->add('photoDe')->add('profildecol')->add('nomDe')->add('prenomDe')->add('tel1De')->add('tel2De')->add('mailDe')->add('qpv')->add('nbrEnfantDe')->add('dateNaissance')->add('trancheAge')->add('villeNaissance')->add('deptNaissance')->add('paysNaissance')->add('nationaliteDe')->add('titreSejourDe')->add('autorisationTravail')->add('locomotion')->add('numPoleEmploiDe')->add('structureRef')->add('allocation')->add('diplomePro')->add('categorieSocio')->add('rqthrqth')->add('adresseadresse')->add('genregenre')->add('metiermetier')->add('niveauetudeniveauetude1')->add('statutstatut')->add('permispermis')->add('useruser')        ;
+        $builder->add('dateInscriptionDe')
+                ->add('dateDerniereModifDe')
+                ->add('photoDe')
+                ->add('profildecol')
+                ->add('nomDe')
+                ->add('prenomDe')
+                ->add('tel1De')
+                ->add('tel2De')
+                ->add('mailDe')
+                ->add('qpv')
+                ->add('nbrEnfantDe')
+                ->add('dateNaissance')
+                ->add('trancheAge')
+                ->add('villeNaissance')
+                ->add('deptNaissance')
+                ->add('paysNaissance')
+                ->add('nationaliteDe')
+                ->add('titreSejourDe')
+                ->add('autorisationTravail')
+                ->add('locomotion')
+                ->add('numPoleEmploiDe')
+                ->add('structureRef')
+                ->add('allocation')
+                ->add('diplomePro')
+                ->add('categorieSocio')
+                ->add('rqthrqth')
+                ->add('adresseadresse')
+                ->add('genregenre')
+                ->add('metiermetier')
+                ->add('niveauetudeniveauetude1')
+                ->add('statutstatut')
+                ->add('permispermis')
+                ->add('useruser')        ;
     }
     
     /**
